dbe41bf93c94f877fe6a815296fb8a7579a1def5
Prakash Rai <prai@mum.edu>
2018-01-27 00:20:27 -0500

    Search flights

--- src/app/flight/flight.model.ts
+++ src/app/flight/flight.model.ts
@@ -1,6 +1,11 @@
 import {BaseModel} from '../base/base.model';
+import {Airport} from '../airport/airport.model';
 
 export class Flight extends BaseModel{
   date: Date;
   price: number;
+  from : Airport;
+  to : Airport;
+  locationFrom : string;
+  locationTo : string;
 }
